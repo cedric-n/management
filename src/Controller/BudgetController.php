f5a6500cf601eddd08dca90a3b149899ecf3fb9b
cedric-n <cedric.ngondo@gmail.com>
2021-06-01 16:39:44 +0200

    create a form for the budget object

--- src/Controller/BudgetController.php
+++ src/Controller/BudgetController.php
@@ -6,6 +6,8 @@ use App\Entity\Budget;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\BudgetType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -34,6 +36,35 @@ class BudgetController extends AbstractController
         );
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("new/", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        $budget = new Budget();
+
+        $form = $this->createForm(BudgetType::class, $budget );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($budget);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('budget_index');
+        }
+
+        return $this->render(
+            'budget/new.html.twig',
+            ["form" => $form->createView()]
+        );
+    }
 
     /**
      * @param string $budgetName
---
+++ src/Form/BudgetType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Budget;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class BudgetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('type', IntegerType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Budget::class,
+        ]);
+    }
+}
